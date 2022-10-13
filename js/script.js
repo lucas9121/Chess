dd8c98435ff1747a459a0c77c0513076cbd3d085
Lucas <lucas2carlos@gmail.com>
2022-10-13 17:05:43 -0400

    adding square names

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('Hello chess')
+const allSquares = document.querySelectorAll('.square')
+
+for(let square of allSquares){
+    console.log(square)
+    let small = document.createElement('small')
+    small.innerHTML = `${square.id}`
+    square.appendChild(small)
+}
