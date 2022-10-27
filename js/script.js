56b033a947b3cb4bbceb1178aea0314d6fe04196
Lucas <lucas2carlos@gmail.com>
2022-10-27 16:34:23 -0400

    adding down function

--- js/script.js
+++ js/script.js
@@ -477,6 +477,48 @@ const up = (idx) => {
     return squares
 }
 
+const down = (idx) => {
+    for(let i = idx + 8; i <= allSquares.length; i += 8){
+        if(!allSquares[i]) break
+        //Player 1 turn
+        if(playerToggle){
+            // player 2 pieces
+            if(allSquares[i].children[0].classList.contains('player2')){
+                squares.push(allSquares[i])
+                changeBackground(allSquares[i], 'green')
+                break
+            // player 1 pieces
+            } else if(allSquares[i].children[0].classList.contains('player1')){
+                changeBackground(allSquares[i], 'red')
+                break
+            // no pieces
+            } else {
+                squares.push(allSquares[i])
+                changeBackground(allSquares[i], '#0010ff99')
+            }
+        // Player 2 turn
+        } else {
+            // player 1 pieces
+            if(allSquares[i].children[0].classList.contains('player1')){
+                squares.push(allSquares[i])
+                changeBackground(allSquares[i], 'green')
+                break
+            // player 2 pieces
+            } else if(allSquares[i].children[0].classList.contains('player2')){
+                changeBackground(allSquares[i], 'red')
+                break
+            // no pieces
+            } else {
+                squares.push(allSquares[i])
+                changeBackground(allSquares[i], '#0010ff99')
+            }
+        }
+    }
+    return squares
+}
+
+
+
 /////////////////////////////////////////////////////////////////////////   KING MOVEMENT   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const kingMovementRules = (idx) => {
